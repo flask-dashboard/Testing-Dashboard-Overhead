3e7501458ecb3f951d045cddea18cb2c41c012c0
flyingbird95 <patrickvogel@live.nl>
2018-03-20 17:40:09 +0100

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,17 @@ docker rm $(docker ps -aq)
 docker build -t webservice .
 
 
-Deploy webservice with the Dashboard
+# Deploy webservice with the Dashboard
 echo "Deploy the webservice with the dashboard"
 docker run -d --name app_with_dashboard -p 9001:9001 -e dashboard=True webservice
-echo "Wait until the webservice is successfully started"
 python test.py http://localhost:9001/ with_dashboard
+
+# Stop previous container
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 
 
 # Deploy webservice without the Dashboard
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
 echo "Deploy the webservice without the dashboard"
 docker run -d --name app_without_dashboard -p 9001:9001 -e dashboard=False webservice
 python test.py http://localhost:9001/ without_dashboard
