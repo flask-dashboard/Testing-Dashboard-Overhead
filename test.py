b46e0d0d64b2376d47f891a321c6b308acb441e8
flyingbird95 <patrickvogel@live.nl>
2018-03-20 14:56:32 +0100

    updated test script

--- test.py
+++ test.py
@@ -21,16 +21,35 @@ def parse_host():
 
 def sleep_until_ready(host):
     """ Waits until the host is up."""
+    now = time.time()
     while True:
         try:
             urllib2.urlopen(host + 'available_languages', timeout=1)
             return
         except Exception:
             time.sleep(1)
-            print('Waiting until host is up')
+            print('Waiting for {} seconds to boot up'.format(time.time() - now))
 
+
+def measure_execution_time(host, page, n):
+    """ Call a certain page n times and returns the execution time (in ms) """
+    data = []
+    for _ in range(n):
+        now = time.time()
+        try:
+            urllib2.urlopen(host + page, timeout=1)
+            return
+        except Exception:
+            print('Can\'t open url {}{}'.format(host, page) )
+        data.append((time.time() - now) * 1000)
+    return data
 
 if __name__ == '__main__':
     host = parse_host()
     sleep_until_ready(host)
     print('Ready for the real testing')
+
+    page = 'available_languages'
+    n = 100
+    data = measure_execution_time(host, page, n)
+    print(data)
