013524d0b2d1f8c39bdca5b8d82ca7d67a0ebcc6
flyingbird95 <patrickvogel@live.nl>
2018-03-21 09:36:39 +0100

    updated readme

--- README.md
+++ README.md
@@ -16,3 +16,8 @@ The Flask application is deployed within a [Docker-image](https://hub.docker.com
 This tries to produce two equivalent execution environments. In one environment the Dashboard is included, in the other it is not.
 
 The difference in results is assumed to be due to the Dashboard.
+
+## What is left ToDo:
+1. Enable monitoring of endpoints in Dashboard (Automatically)
+2. Determine which (and how often) endpoints are tested?
+3. Automatically compare the results of both output-files
---
+++ util.py
@@ -0,0 +1,34 @@
+"""
+This file contains a number of helper functions
+"""
+
+import sys
+import os
+
+
+def parse_args():
+    """ Returns the host (as a string) and the name from the script arguments. """
+    try:
+        return sys.argv[1], sys.argv[2]
+    except Exception as e:
+        print('Got exception: {}'.format(e))
+        print('Usage: {} {{Host}} {{Name}}'.format(sys.argv[0]))
+        print('Given: {} {} {}'.format(sys.argv[0], sys.argv[1], sys.argv[2]))
+        sys.exit(1)
+
+
+def make_directory(name):
+    """ Creates a directory if it doesn't exists """
+    try:
+        os.makedirs(name)
+    except Exception as e:
+        print(e)
+
+
+def save_result(data, name):
+    """ Saves the result in a file """
+    make_directory('output')
+
+    with open('output/{}'.format(name), 'w') as file:
+        for line in data:
+            file.write(str(line) + '\n')
