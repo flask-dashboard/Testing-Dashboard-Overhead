34838565e4dbb016218ffb093dc8006ec1f72e3d
Thijs Klooster <thijskloostert@gmail.com>
2018-03-21 19:22:21 +0100

    Monitor all endpoints

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ Run this script for making a number of requests to the webservice
 import sys
 import requests
 import time
+import sqlite3
 import urllib2
 from util import parse_args, save_result
 
@@ -22,6 +23,19 @@ def sleep_until_ready(host):
             sys.stdout.flush()
 
 
+def monitor_all_endpoints(db):
+    """ Enables the monitoring of all endpoints."""
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute("UPDATE rules SET monitor = 1")
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print('Could not enable the monitoring of all endpoints.')
+        print(e)
+
+
 def measure_execution_time(host, page, n=100):
     """ Call a certain page n times and returns the execution time (in ms) """
     data = []
@@ -37,7 +51,9 @@ def measure_execution_time(host, page, n=100):
 if __name__ == '__main__':
     host, name = parse_args()
     sleep_until_ready(host)
-    print('Ready for testing the webservice')
-
+    print('Host is up.\nEnabling monitoring of all endpoints...')
+    monitor_all_endpoints('/Zeeguu-API/flask_monitoringdashboard.db')
+    print('All endpoints are now monitored.\nTesting the overhead now...')
     data = measure_execution_time(host, page='available_languages')
     save_result(data, name + '.txt')
+    print('Results saved.')
