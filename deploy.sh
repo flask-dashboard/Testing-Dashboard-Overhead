ee8df80e0289db2383fd8688596966aaede860ae
Mircea Lungu <mircea.lungu@gmail.com>
2018-04-06 16:41:58 +0200

    DRY...

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Use this script for starting everything
 
+
 echo "Stop and remove running containers"
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
@@ -8,12 +9,14 @@ docker rm $(docker ps -aq)
 # Build the webservice
 docker build -t webservice .
 
+HOST_PORT=8801
+
 # Deploy webservice with the Dashboard
 echo "Deploy the webservice with the dashboard (and outliers)"
-docker run -d --name with_dashboard_and_outliers -p 9001:9001 \
+docker run -d --name with_dashboard_and_outliers -p $HOST_PORT:9001\
 	-e dashboard=True \
 	-e outlier="0" webservice
-python -m testing http://localhost:9001/ with_dashboard_and_outliers
+python -m testing http://localhost:$HOST_PORT/ with_dashboard_and_outliers
 
 Stop previous container
 docker stop $(docker ps -aq)
@@ -21,10 +24,10 @@ docker rm $(docker ps -aq)
 
 # Deploy webservice with the Dashboard
 echo "Deploy the webservice with the dashboard (but without outliers)"
-docker run -d --name with_dashboard_but_no_outliers -p 9001:9001 \
+docker run -d --name with_dashboard_but_no_outliers -p $HOST_PORT:9001\
 	-e dashboard=True \
 	-e outlier="10000" webservice
-python -m testing http://localhost:9001/ with_dashboard_but_no_outliers
+python -m testing http://localhost:$HOST_PORT/ with_dashboard_but_no_outliers
 
 # Stop previous containers
 docker stop $(docker ps -aq)
@@ -32,10 +35,10 @@ docker rm $(docker ps -aq)
 
 # Deploy webservice without the Dashboard
 echo "Deploy the webservice without the dashboard"
-docker run -d --name without_dashboard -p 9001:9001 \
+docker run -d --name without_dashboard -p $HOST_PORT:9001\
 	-e dashboard=False \
 	-e outlier="2.5" webservice
-python -m testing http://localhost:9001/ without_dashboard
+python -m testing http://localhost:$HOST_PORT/ without_dashboard
 
 # Stop all containers
 docker stop $(docker ps -aq)
