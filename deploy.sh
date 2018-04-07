26accd6e1d64b36bc2f093fafd85c8b21cd77077
flyingbird95 <patrickvogel@live.nl>
2018-04-07 18:25:03 +0200

    fix in using vars

--- deploy.sh
+++ deploy.sh
@@ -14,33 +14,33 @@ docker build -t webservice .
 
 # Deploy webservice with the Dashboard
 echo "Deploy the three webservices one by one"
-docker run -d --name with_dashboard_and_outliers -p PORT_A:9001 \
+docker run -d --name with_dashboard_and_outliers -p $PORT_A:9001 \
 	-e dashboard=True \
 	-e with_outliers=True \
 	-e outlier="0" webservice
-python -m testing http://localhost:PORT_A/ with_dashboard_and_outliers
+python -m testing http://localhost:$PORT_A/ with_dashboard_and_outliers
 
 
 # Stop running container
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 
-docker run -d --name with_dashboard_but_no_outliers -p PORT_B:9001 \
+docker run -d --name with_dashboard_but_no_outliers -p $PORT_B:9001 \
 	-e dashboard=True \
 	-e with_outliers=False \
 	-e outlier="10000" webservice
-python -m testing http://localhost:PORT_B/ with_dashboard_but_no_outliers
+python -m testing http://localhost:$PORT_B/ with_dashboard_but_no_outliers
 
 
 # Stop running container
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 
-docker run -d --name without_dashboard -p PORT_C:9001 \
+docker run -d --name without_dashboard -p $PORT_C:9001 \
 	-e dashboard=False \
 	-e with_outliers=False \
 	-e outlier="2.5" webservice
-python -m testing http://localhost:PORT_C/ without_dashboard
+python -m testing http://localhost:$PORT_C/ without_dashboard
 
 # Stop previous container
 docker stop $(docker ps -aq)
--- deploy_parallel.sh
+++ deploy_parallel.sh
@@ -15,22 +15,22 @@ docker build -t webservice .
 
 # Deploy webservice with the Dashboard
 echo "Deploy the three webservices all at once"
-docker run -d --name with_dashboard_and_outliers -p PORT_A:9001 \
+docker run -d --name with_dashboard_and_outliers -p $PORT_A:9001 \
 	-e dashboard=True \
 	-e with_outliers=True \
 	-e outlier="0" webservice
-docker run -d --name with_dashboard_but_no_outliers -p PORT_B:9001 \
+docker run -d --name with_dashboard_but_no_outliers -p $PORT_B:9001 \
 	-e dashboard=True \
 	-e with_outliers=False \
 	-e outlier="10000" webservice
-docker run -d --name without_dashboard -p PORT_C:9001 \
+docker run -d --name without_dashboard -p $PORT_C:9001 \
 	-e dashboard=False \
 	-e with_outliers=False \
 	-e outlier="2.5" webservice
 
-python -m testing http://localhost:PORT_A/ with_dashboard_and_outliers \
-				  http://localhost:PORT_B/ with_dashboard_but_no_outliers \
-				  http://localhost:PORT_C/ without_dashboard
+python -m testing http://localhost:$PORT_A/ with_dashboard_and_outliers \
+				  http://localhost:$PORT_B/ with_dashboard_but_no_outliers \
+				  http://localhost:$PORT_C/ without_dashboard
 
 # Stop previous container
 docker stop $(docker ps -aq)
